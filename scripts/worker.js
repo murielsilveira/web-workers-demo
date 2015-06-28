4357070cd6944ed33dcefb0a4dc2d8d08c5b5e48
Muriel Silveira <murielkong@gmail.com>
2015-06-28 00:24:17 -0300

    Get the function to manipulate pixels just one time. That improves a lot the performance.

--- scripts/worker.js
+++ scripts/worker.js
@@ -5,13 +5,14 @@ this.onmessage = function(e) {
   var type = e.data.type;
 
   try {
+    var manipulate = getManipulateFunction(type);
     length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
       b = imageData.data[i * 4 + 2];
       a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
+      pixel = manipulate(r, g, b, a);
       imageData.data[i * 4 + 0] = pixel[0];
       imageData.data[i * 4 + 1] = pixel[1];
       imageData.data[i * 4 + 2] = pixel[2];
